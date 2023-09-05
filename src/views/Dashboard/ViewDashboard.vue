ad94d53028c8a674f5ab73ac16b23019cfc73ce2
mcgmatheus <matheus.mcg@hotmail.com.br>
2023-09-04 23:57:23 -0300

    feat: adiciona saudacao e cards

--- src/views/Dashboard/ViewDashboard.vue
+++ src/views/Dashboard/ViewDashboard.vue
@@ -12,10 +12,10 @@
       <router-link to="/home">
         <v-btn class="text-white">Home</v-btn>
       </router-link>
-      <router-link to="/home">
+      <router-link to="/gerenciamento_alunos">
         <v-btn class="text-white">Alunos</v-btn>
       </router-link>
-      <router-link to="/home">
+      <router-link to="/gerenciamento_exercicios">
         <v-btn class="text-white">Exercícios</v-btn>
       </router-link>
       <router-link to="/home">
@@ -26,7 +26,37 @@
     <v-main>
       <v-container>
         <v-row>
-          <v-col>teste</v-col>
+          <v-col><h1>Bem vindo, nome</h1></v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="5">
+            <v-card>
+              <div class="d-flex">
+                <v-card-text class="d-flex flex-column">
+                  <p class="pa-3">qtd Alunos</p>
+                  <router-link to="/cadastro_novo_aluno">
+                    <v-btn color="#2f157e" class="text-white">Adicionar</v-btn>
+                  </router-link> </v-card-text
+                ><v-card-text>
+                  <v-img src="/icons8-users.png" width="64px"></v-img>
+                </v-card-text>
+              </div>
+            </v-card>
+          </v-col>
+          <v-col cols="5">
+            <v-card>
+              <div class="d-flex">
+                <v-card-text class="d-flex flex-column">
+                  <p class="pa-3">qtd Exercícios</p>
+                  <router-link to="/cadastro_novo_treino">
+                    <v-btn color="#2f157e" class="text-white">Adicionar</v-btn>
+                  </router-link> </v-card-text
+                ><v-card-text>
+                  <v-img src="/icons8-strong.png" width="64px"></v-img>
+                </v-card-text>
+              </div>
+            </v-card>
+          </v-col>
         </v-row>
       </v-container>
     </v-main>
