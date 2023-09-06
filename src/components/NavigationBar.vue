fe8b468bd64b0be4e2d103249c998bc4fac3e387
mcgmatheus <matheus.mcg@hotmail.com.br>
2023-09-05 22:46:29 -0300

    feat: cria componente NavigationBar

---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app-bar color="#2f157e" class="text-white">
+    <template v-slot:prepend>
+      <v-app-bar-nav-icon>
+        <router-link to="/home">
+          <v-img src="/TrainSysLogo.png"></v-img>
+        </router-link>
+      </v-app-bar-nav-icon>
+      <v-app-bar-title>TrainSys</v-app-bar-title>
+    </template>
+    <router-link to="/home">
+      <v-btn class="text-white">Home</v-btn>
+    </router-link>
+    <router-link to="/gerenciamento_alunos">
+      <v-btn class="text-white">Alunos</v-btn>
+    </router-link>
+    <router-link to="/gerenciamento_exercicios">
+      <v-btn class="text-white">Exercícios</v-btn>
+    </router-link>
+    <router-link to="/home">
+      <v-btn class="text-white">Sair</v-btn>
+    </router-link>
+  </v-app-bar>
+</template>
+
+<script></script>
+
+<style></style>
--- src/views/Dashboard/ViewDashboard.vue
+++ src/views/Dashboard/ViewDashboard.vue
@@ -1,28 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar color="#2f157e" class="text-white">
-      <template v-slot:prepend>
-        <v-app-bar-nav-icon>
-          <router-link to="/home">
-            <v-img src="/TrainSysLogo.png"></v-img>
-          </router-link>
-        </v-app-bar-nav-icon>
-        <v-app-bar-title>TrainSys</v-app-bar-title>
-      </template>
-      <router-link to="/home">
-        <v-btn class="text-white">Home</v-btn>
-      </router-link>
-      <router-link to="/gerenciamento_alunos">
-        <v-btn class="text-white">Alunos</v-btn>
-      </router-link>
-      <router-link to="/gerenciamento_exercicios">
-        <v-btn class="text-white">Exercícios</v-btn>
-      </router-link>
-      <router-link to="/home">
-        <v-btn class="text-white">Sair</v-btn>
-      </router-link>
-    </v-app-bar>
-
+    <NavigationBar></NavigationBar>
     <v-main>
       <v-container>
         <v-row>
@@ -67,6 +45,7 @@
 
 <script>
 import axios from 'axios'
+import NavigationBar from '../../components/NavigationBar.vue'
 export default {
   data() {
     return {
@@ -74,6 +53,9 @@ export default {
       amountStudents: '',
       amountExercises: ''
     }
+  },
+  components: {
+    NavigationBar
   },
   mounted() {
     const storedUserInfo = localStorage.getItem('userInfo')
