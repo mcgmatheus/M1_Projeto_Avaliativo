02c9ec1d9f14df4fe77d22703b09b67fa49e3503
mcgmatheus <matheus.mcg@hotmail.com.br>
2023-09-08 00:29:24 -0300

    Inicia pagina de gerenciamento de exercicios

--- src/views/ExerciseManagement/ExerciseManagement.vue
+++ src/views/ExerciseManagement/ExerciseManagement.vue
@@ -1,7 +1,78 @@
 <template>
-  <div></div>
+  <v-app>
+    <NavigationBar></NavigationBar>
+    <v-main>
+      <v-container>
+        <v-row class="mb-2 mt-4">
+          <h2>Exercícios</h2>
+          <v-divider :thickness="2"></v-divider>
+        </v-row>
+        <v-row>
+          <v-col cols="12">
+            <v-card>
+              <v-form class="ma-5">
+                <v-row class="text-center">
+                  <v-col cols="9">
+                    <v-text-field label="Digite o nome do exercício" type="text"></v-text-field>
+                  </v-col>
+                  <v-col cols="3">
+                    <v-btn class="mt-2">Cadastrar</v-btn>
+                  </v-col>
+                </v-row>
+              </v-form>
+            </v-card>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="12">
+            <v-card>
+              <v-table fixed-header height="270px">
+                <thead>
+                  <tr>
+                    <th class="text-left">Nome do exercício</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr v-for="exercise in exercises" :key="exercise.name">
+                    <td>{{ exercise.name }}</td>
+                  </tr>
+                </tbody>
+              </v-table>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
-<script></script>
+<script>
+// import axios from 'axios'
+import NavigationBar from '../../components/NavigationBar.vue'
 
-<style></style>
+export default {
+  data() {
+    return {
+      exercises: [
+        { name: 'supino reto' },
+        { name: 'supino obliquo' },
+        { name: 'agachamento livre' },
+        { name: 'supino reto' },
+        { name: 'supino obliquo' },
+        { name: 'agachamento livre' },
+        { name: 'supino reto' },
+        { name: 'supino obliquo' },
+        { name: 'agachamento livre' },
+        { name: 'supino reto' },
+        { name: 'supino obliquo' },
+        { name: 'agachamento livre' }
+      ]
+    }
+  },
+  components: {
+    NavigationBar
+  }
+}
+</script>
+
+<style scoped></style>
