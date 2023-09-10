65381e522706e098fefc93fc6688b2db482c4298
mcgmatheus <matheus.mcg@hotmail.com.br>
2023-09-09 23:43:53 -0300

    Feat: tabela de exibição de alunos

--- src/views/StudentsManagement/StudentsManagement.vue
+++ src/views/StudentsManagement/StudentsManagement.vue
@@ -12,7 +12,7 @@
           <v-divider :thickness="2"></v-divider>
         </v-row>
         <v-row>
-          <v-col cols="12">
+          <v-col>
             <v-card>
               <v-form class="ma-5">
                 <v-row class="text-center">
@@ -32,14 +32,23 @@
             <v-card>
               <v-table fixed-header height="270px">
                 <thead>
-                  <tr>
-                    <th class="text-left">Nome do aluno</th>
-                  </tr>
+                  <th class="colNames">Nome do aluno</th>
+                  <th class="colActions">Ações</th>
                 </thead>
                 <tbody>
-                  <!-- <tr v-for="exercise in exercises" :key="exercise.name">
-                    <td>{{ exercise.description }}</td>
-                  </tr> -->
+                  <tr v-for="student in students" :key="student.name">
+                    <td>{{ student.name }}</td>
+                    <td class="text-right">
+                      <router-link to="/cadastro_novo_treino"
+                        ><v-btn class="mr-8" color="primary" density="compact"
+                          >Montar treino</v-btn
+                        ></router-link
+                      >
+                      <router-link to="/ver_treino"
+                        ><v-btn color="primary" density="compact">Ver treino</v-btn></router-link
+                      >
+                    </td>
+                  </tr>
                 </tbody>
               </v-table>
             </v-card>
@@ -55,7 +64,19 @@ import NavigationBar from '../../components/NavigationBar.vue'
 
 export default {
   data() {
-    return {}
+    return {
+      search: '',
+      students: [
+        { name: 'João' },
+        { name: 'Ana' },
+        { name: 'Maria' },
+        { name: 'Pedro' },
+        { name: 'João' },
+        { name: 'Ana' },
+        { name: 'Maria' },
+        { name: 'Pedro' }
+      ]
+    }
   },
   components: {
     NavigationBar
@@ -63,4 +84,9 @@ export default {
 }
 </script>
 
-<style></style>
+<style scoped>
+.colNames,
+.colActions {
+  width: 50%;
+}
+</style>
