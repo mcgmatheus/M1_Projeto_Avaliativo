54d76306a0f1f1173d0155c9a2d6a2ae42ca5475
mcgmatheus <matheus.mcg@hotmail.com.br>
2023-09-16 12:54:03 -0300

    Feat: cria tabela de exibição de exercicios
    Por enquanto, está utilizando o array de objetos AllWorkouts fixo.

--- src/views/ViewTraining/ViewTraining.vue
+++ src/views/ViewTraining/ViewTraining.vue
@@ -3,6 +3,10 @@
     <NavigationBar></NavigationBar>
     <v-main>
       <v-container>
+        {{ student_id }}
+        {{ allWorkouts }}
+        {{ dayOfWeek }}
+        {{ todayStudentWorkout }}
         <v-card
           ><v-card-title>
             <h2>Treino de Hoje</h2>
@@ -21,10 +25,10 @@
               </thead>
               <tbody>
                 <tr v-for="exercise in todayStudentWorkout" :key="exercise.name">
-                  <td>{{ exercise.name }}</td>
+                  <td>{{ exercise.exerciseDescription }}</td>
                   <td>{{ exercise.repetitions }}</td>
                   <td>{{ exercise.weight }}</td>
-                  <td>{{ exercise.break_time }}</td>
+                  <td>{{ exercise.break_time }} segundos</td>
                   <td>{{ exercise.observations }}</td>
                 </tr>
               </tbody>
@@ -38,17 +42,110 @@
 
 <script>
 import NavigationBar from '../../components/NavigationBar.vue'
-// import axios from 'axios'
+import axios from 'axios'
 
 export default {
   data() {
-    return {}
+    return {
+      student_id: this.$route.params.id,
+      todayStudentWorkout: [],
+      dayOfWeek: this.getCurrentDay(new Date().getDay()),
+      allWorkouts: [
+        {
+          id: 1,
+          exerciseDescription: 'supino',
+          repetitions: 12,
+          weight: 30,
+          break_time: 45,
+          day: 'segunda'
+        },
+        {
+          id: 1,
+          exerciseDescription: 'supino2',
+          repetitions: 12,
+          weight: 40,
+          break_time: 45,
+          day: 'terca'
+        },
+        {
+          id: 1,
+          exerciseDescription: 'supino3',
+          repetitions: 12,
+          weight: 50,
+          break_time: 45,
+          day: 'quarta'
+        },
+        {
+          id: 1,
+          exerciseDescription: 'supino4',
+          repetitions: 12,
+          weight: 60,
+          break_time: 45,
+          day: 'quinta'
+        },
+        {
+          id: 1,
+          exerciseDescription: 'supino5',
+          repetitions: 12,
+          weight: 70,
+          break_time: 45,
+          day: 'sexta'
+        },
+        {
+          id: 1,
+          exerciseDescription: 'supino6',
+          repetitions: 12,
+          weight: 80,
+          break_time: 45,
+          day: 'sabado'
+        },
+        {
+          id: 1,
+          exerciseDescription: 'supino7',
+          repetitions: 12,
+          weight: 90,
+          break_time: 45,
+          day: 'domingo'
+        },
+        {
+          id: 1,
+          exerciseDescription: 'flexão6',
+          repetitions: 25,
+          weight: 40,
+          break_time: 60,
+          day: 'sabado'
+        }
+      ]
+    }
   },
   components: {
     NavigationBar
   },
+  created() {
+    this.getTodayWorkout()
+  },
+  methods: {
+    getCurrentDay(value) {
+      const dayOptions = [
+        { value: 'segunda', number: 1 },
+        { value: 'terca', number: 2 },
+        { value: 'quarta', number: 3 },
+        { value: 'quinta', number: 4 },
+        { value: 'sexta', number: 5 },
+        { value: 'sabado', number: 6 },
+        { value: 'domingo', number: 0 }
+      ]
 
-  methods: {}
+      this.dayOfWeek = dayOptions.find((item) => item.number === value)
+      return this.dayOfWeek.value
+    },
+    getTodayWorkout() {
+      this.todayStudentWorkout = this.allWorkouts.filter(
+        (workout) => workout.day === this.dayOfWeek
+      )
+      return this.todayStudentWorkout
+    }
+  }
 }
 </script>
 
