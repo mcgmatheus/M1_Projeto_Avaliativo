eac2408f492bf3bae1b2163ec9e1359e3c8eaa04
mcgmatheus <matheus.mcg@hotmail.com.br>
2023-09-16 09:58:11 -0300

    Inicia pagina de visualicao de treino

--- src/views/ViewTraining/ViewTraining.vue
+++ src/views/ViewTraining/ViewTraining.vue
@@ -1,7 +1,55 @@
 <template>
-  <div></div>
+  <v-app>
+    <NavigationBar></NavigationBar>
+    <v-main>
+      <v-container>
+        <v-card
+          ><v-card-title>
+            <h2>Treino de Hoje</h2>
+            <v-divider :thickness="2"></v-divider>
+          </v-card-title>
+          <v-card-text>
+            <v-table>
+              <thead>
+                <tr>
+                  <th class="text-left">Exercicio</th>
+                  <th class="text-left">Repetições</th>
+                  <th class="text-left">Carga</th>
+                  <th class="text-left">Pausa</th>
+                  <th class="text-left">Observações</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="exercise in todayStudentWorkout" :key="exercise.name">
+                  <td>{{ exercise.name }}</td>
+                  <td>{{ exercise.repetitions }}</td>
+                  <td>{{ exercise.weight }}</td>
+                  <td>{{ exercise.break_time }}</td>
+                  <td>{{ exercise.observations }}</td>
+                </tr>
+              </tbody>
+            </v-table>
+          </v-card-text>
+        </v-card>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
-<script></script>
+<script>
+import NavigationBar from '../../components/NavigationBar.vue'
+// import axios from 'axios'
 
-<style></style>
+export default {
+  data() {
+    return {}
+  },
+  components: {
+    NavigationBar
+  },
+
+  methods: {}
+}
+</script>
+
+<style scoped></style>
